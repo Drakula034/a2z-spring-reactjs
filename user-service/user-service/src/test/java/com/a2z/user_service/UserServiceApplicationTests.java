92c122029442f49fe18a2fcaf85541298fe8a123
Drakula034 <abhishek034nits@gmail.com>
2024-06-10 10:54:53 +0530

    created this project and added user-service microservice

---
+++ user-service/user-service/src/test/java/com/a2z/user_service/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a2z.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
